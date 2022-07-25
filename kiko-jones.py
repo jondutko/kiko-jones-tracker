46da72bee69b0fc348d647b6a649c280f68e1d91
jondutko <jonathan.dutko@gmail.com>
2022-07-25 00:22:09 -0400

    Add refresh endpoint; 20 -> 100 games

--- kiko-jones.py
+++ kiko-jones.py
@@ -45,64 +45,74 @@ RIOT_KEY = os.environ['RIOT_API_KEY']
 summoner_name = "KIKO JONES"
 summoner_name_html_safe = "KIKO%20JONES"
 
-r = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summoner_name_html_safe+"?api_key="+RIOT_KEY)
-
-rjson = r.json()
-
-puuid = rjson["puuid"]
-summid = rjson["id"]
-
-r = requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=20&api_key="+RIOT_KEY)
-
-matches = r.json()
-
-i = 0
-total_kda = 0
-total_gpm = 0
-total_win = 0
-
-for match in matches:
-	game = Game()
-	print ("Processing "+match+" match id")
-
-	r = requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/"+match+"?api_key="+RIOT_KEY)
+def process_matches():
+	g  = []
+	r = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summoner_name_html_safe+"?api_key="+RIOT_KEY)
 
 	rjson = r.json()
 
-	game.minutes = round(rjson["info"]["gameDuration"]/60)
-	unix_timestamp = rjson["info"]["gameEndTimestamp"]
-	unix_timestamp = unix_timestamp/1000
-	game.date = datetime.fromtimestamp(unix_timestamp).strftime('%a %b %d')
+	puuid = rjson["puuid"]
+	summid = rjson["id"]
 
-	for participant in rjson["info"]["participants"]:
-		if (participant["summonerName"] == summoner_name):
-			game.win = participant["win"]
-			game.championName = participant["championName"]
-			game.kills = participant["kills"]
-			game.assists = participant["assists"]
-			game.deaths = participant["deaths"]
-			kda = (participant["kills"]+participant["assists"])/max(participant["deaths"], 1)
-			game.kda = round(kda, 1)
-			game.gpm = round(participant["challenges"]["goldPerMinute"],1)
-			i = i + 1
-			total_gpm = total_gpm + participant["challenges"]["goldPerMinute"]
-			total_kda = total_kda + kda
-			if game.win:
-				total_win = total_win + 1
+	r = requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=100&api_key="+RIOT_KEY)
 
-	Games.append(game)
+	matches = r.json()
 
-wr = round(total_win/i, 2) * 100
-avg_kda = round(total_kda/i, 1)
-avg_gpm = round(total_gpm/i, 1)
+	i = 0
+	total_kda = 0
+	total_gpm = 0
+	total_win = 0
 
-print (str(total_win)+" wins\t"+str(wr)+"%")
-print ("avg kda: "+str(avg_kda))
-print ("avg gpm: "+str(avg_gpm))
-	
+	for match in matches:
+		game = Game()
+		print ("Processing "+match+" match id")
+
+		r = requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/"+match+"?api_key="+RIOT_KEY)
+
+		rjson = r.json()
+
+		game.minutes = round(rjson["info"]["gameDuration"]/60)
+		unix_timestamp = rjson["info"]["gameEndTimestamp"]
+		unix_timestamp = unix_timestamp/1000
+		game.date = datetime.fromtimestamp(unix_timestamp).strftime('%a %b %d')
+
+		for participant in rjson["info"]["participants"]:
+			if (participant["summonerName"] == summoner_name):
+				game.win = participant["win"]
+				game.championName = participant["championName"]
+				game.kills = participant["kills"]
+				game.assists = participant["assists"]
+				game.deaths = participant["deaths"]
+				kda = (participant["kills"]+participant["assists"])/max(participant["deaths"], 1)
+				game.kda = round(kda, 1)
+				game.gpm = round(participant["challenges"]["goldPerMinute"],1)
+				i = i + 1
+				total_gpm = total_gpm + participant["challenges"]["goldPerMinute"]
+				total_kda = total_kda + kda
+				if game.win:
+					total_win = total_win + 1
+
+		g.append(game)
+
+	wr = round(total_win/i, 2) * 100
+	avg_kda = round(total_kda/i, 1)
+	avg_gpm = round(total_gpm/i, 1)
+
+	print (str(total_win)+" wins\t"+str(wr)+"%")
+	print ("avg kda: "+str(avg_kda))
+	print ("avg gpm: "+str(avg_gpm))
+
+	return g
+
+Games = process_matches()
+
+@app.route('/refresh')
+def refresh():
+	Games = process_matches()
+	return "Games set refreshed"
+
 @app.route('/')
 def match_history():
-
 	
 	r = requests.get("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+summid+"?api_key="+RIOT_KEY)
 	rjson = r.json()
