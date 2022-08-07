9a2c6333d4727fcb67656bf6a8eb1f5a120eddc9
jondutko <jonathan.dutko@gmail.com>
2022-08-07 18:29:10 -0400

    Match to HTML

--- kiko-jones.py
+++ kiko-jones.py
@@ -6,17 +6,26 @@ import time
 
 app = Flask(__name__)
 
+def match_to_html(match):
+	r = ""
+	if match[1]:
+		r = r + "<font color=\"cornflowerblue\">WIN"
+	else:
+		r = r + "<font color=\"coral\">LOSS"
+	r = r + "</font></br>"
+	return r
+
 @app.route('/')
 def match_history():
 	GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 	SHEET_ID = os.environ['SHEET_ID']
 
-	headers = {'Authorization': 'Bearer '+GOOGLE_API_KEY}
+	headers = {'Authorization': 'Bearer '+api_key}
 	spreadsheet = requests.get("https://sheets.googleapis.com/v4/spreadsheets/"+SHEET_ID+"/values/A1:F20?key="+GOOGLE_API_KEY)
 	data = spreadsheet.json()
 	matches = data["values"]
+	r = ""
 	for match in matches:
-		for item in match:
-			print (item)
+		r = r + match_to_html(match)
 
-	return "Check the Heroku logs."
+	return r
