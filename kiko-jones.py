4082444937c13a5b7535fd105544b813bb300d85
jondutko <jonathan.dutko@gmail.com>
2022-08-07 18:21:52 -0400

    Remove API keys

--- kiko-jones.py
+++ kiko-jones.py
@@ -8,10 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def match_history():
-	#GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
-	GOOGLE_API_KEY = "AIzaSyCnX0UZyag_7Kfx0mLyblr7WnF_SqWluTE"
-	#SHEET_ID = os.environ['SHEET_ID']
-	SHEET_ID = "14GD1vi82SRnKD5aVw-q4PcQF3t895HPd2OjS2krpPsc"
+	GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+	SHEET_ID = os.environ['SHEET_ID']
 
 	headers = {'Authorization': 'Bearer '+api_key}
 	spreadsheet = requests.get("https://sheets.googleapis.com/v4/spreadsheets/"+SHEET_ID+"/values/A1:F20?key="+GOOGLE_API_KEY)
