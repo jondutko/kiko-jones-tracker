2bf695a7679becbb8ae940f1131fc16e32599db9
jondutko <jonathan.dutko@gmail.com>
2022-08-19 19:35:58 -0400

    Runes time

--- kiko-jones.py
+++ kiko-jones.py
@@ -8,8 +8,20 @@ import time
 app = Flask(__name__)
 Bootstrap(app)
 
+class Rune:
+	def __init__(self, id, runesreforged):
+		self.id = id
+		self.json = ""
+		for category in runesreforged:
+			for slot in category:
+				if slot["id"] == self.id:
+					self.json = slot
+		self.name = self.json.name
+		self.icon = self.json.icon
+		
+
 class Game:
-	def __init__(self, match):
+	def __init__(self, match, runesreforged):
 		self.matchID = match[0]
 		self.timestamp = match[1]
 		self.date = datetime.fromtimestamp(int(match[1])/1000).strftime('%a %b %d')
@@ -29,6 +41,14 @@ class Game:
 		self.items.append(match[12])
 		self.items.append(match[13])
 		self.items.append(match[14])
+		self.runes = []
+		self.runes.append(Rune(match[15]))
+		self.runes.append(Rune(match[16]))
+		self.runes.append(Rune(match[17]))
+		self.runes.append(Rune(match[18]))
+		self.runes.append(Rune(match[19]))
+		self.runes.append(Rune(match[20]))
+		print (self.runes[0].name+" "+self.runes[0].icon)
 	
 
 @app.route('/')
@@ -37,11 +57,13 @@ def match_history():
 	SHEET_ID = os.environ['SHEET_ID']
 
 	headers = {'Authorization': 'Bearer '+GOOGLE_API_KEY}
-	spreadsheet = requests.get("https://sheets.googleapis.com/v4/spreadsheets/"+SHEET_ID+"/values/A1:P20?key="+GOOGLE_API_KEY)
+	spreadsheet = requests.get("https://sheets.googleapis.com/v4/spreadsheets/"+SHEET_ID+"/values/A1:V20?key="+GOOGLE_API_KEY)
 	data = spreadsheet.json()
+	runesreforged_response = requests.get("http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/runesReforged.json")
+	runesreforged = runesreforged_response.json()
 	matches = data["values"]
 	Games = []
 	for match in matches:
-		g = Game(match)
+		g = Game(match, runesreforged)
 		Games.append(g)
 	return render_template('index.html', games=Games)
