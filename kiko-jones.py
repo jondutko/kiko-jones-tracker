673c7f18db181d0c97d5f9ef511018b25277efe5
jondutko <jonathan.dutko@gmail.com>
2022-08-20 00:20:26 -0400

    100 games...

--- kiko-jones.py
+++ kiko-jones.py
@@ -77,7 +77,7 @@ def match_history():
 	SHEET_ID = os.environ['SHEET_ID']
 
 	headers = {'Authorization': 'Bearer '+GOOGLE_API_KEY}
-	spreadsheet = requests.get("https://sheets.googleapis.com/v4/spreadsheets/"+SHEET_ID+"/values/A1:V50?key="+GOOGLE_API_KEY)
+	spreadsheet = requests.get("https://sheets.googleapis.com/v4/spreadsheets/"+SHEET_ID+"/values/A1:V100?key="+GOOGLE_API_KEY)
 	data = spreadsheet.json()
 	matches = data["values"]
 	Games = []
